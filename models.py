57324a55aec50f0a0bbcafacf222a664c0350554
dudayraj2005-byte <dudayraj2005@gmail.com>
2026-03-23 21:46:28 +0530

    models.py

--- models.py
+++ models.py
@@ -5,8 +5,6 @@ class Ticket(Base):
     __tablename__ = "tickets"
 
     id = Column(Integer, primary_key=True, index=True)
-    patient_name = Column(String)
-    age = Column(Integer)
     user_message = Column(Text)
     category = Column(String)
     severity = Column(String)
